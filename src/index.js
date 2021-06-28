cbb147e41ce28b704755a9b2f355313db141da48
unknown <mayercandelo.1985@gmail.com>
2021-06-28 14:18:41 -0500

    progreso 28 de junio

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import validator from './validator.js';
 
-function mostrarCarrito(){
-   var documento= document.getElementsByClassName("carrito")
+/*function mostrarCarrito(){
+   document.getElementsByClassName("carrito").style.display="block"
     documento.style.display="block";
-}
+}*/
+const tarjeta = document.querySelector("#tarjeta"),
+      botonabrir=document.querySelector("#btn-formulario");
+
+tarjeta.addEventListener('click', () => {
+    tarjeta.classList.toggle('active');
+
+});
+
+botonabrir.addEventListener('click', ()=> {
+    botonabrir.classList.toggle('active')
+})
+
+
+
+
 
 console.log(validator);
